4bb06f6cf7866d215adb44c109d75a47e9a34334
User Name <annakirath@gmail.com>
2024-08-05 16:19:50 +0200

    pulling data from dev

--- local-gem/src/App.jsx
+++ local-gem/src/App.jsx
@@ -2,8 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-//import SignupForm from './components/SignupForm/SignupForm';
-//import * as authService from '../src/services/authService';
 
 function App() {
   const [count, setCount] = useState(0)
