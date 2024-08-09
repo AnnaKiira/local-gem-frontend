4a9621fee712b4a6421d43c34b3d37400dbc46f2
Hanna Lewis <hannaelisabethlewis@gmail.com>
2024-08-09 09:26:54 +0100

    style changes

--- local-gem/src/components/Places/PlacesList.jsx
+++ local-gem/src/components/Places/PlacesList.jsx
@@ -1,4 +1,7 @@
 import { Link } from 'react-router-dom'
+import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
 
 const PlaceList = ({places}) => {
   console.log(places)
