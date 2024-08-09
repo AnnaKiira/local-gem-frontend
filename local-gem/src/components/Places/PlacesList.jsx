69a0047c90e377163082754c93b36d39ba8166db
User Name <annakirath@gmail.com>
2024-08-09 10:43:50 +0100

    updating placeslist

--- local-gem/src/components/Places/PlacesList.jsx
+++ local-gem/src/components/Places/PlacesList.jsx
@@ -1,34 +1,35 @@
 import { Link } from 'react-router-dom'
 import './PlacesList.scss'
 
+
 const PlaceList = ({ places }) => {
-  console.log(places)
+ console.log(places)
 
 
-  return (
-    <main className="places-list">
-      {places.map((place) => (
-        <Link key={place._id} to={`/places/${place._id}`} className="place-card-link">
-          <article className="place-card">
-            <header>
-              <h2>{place.placeName}</h2>
-            </header>
-            <p>{place.description}</p>
-            {place.image && (
-              <div className="upload-image" style={{ backgroundImage: `url(${place.image})` }}> </div>
-            )}
-          </article>
-        </Link>
-        </Col>
-      )
-          })
-    :
-    <h2> No posts to display.</h2>
-  }
-      </Row>
-     </Container>
-    </main>
-  )
+
+
+ return (
+   <main className="places-list">
+     {places.map((place) => (
+       <Link key={place._id} to={`/places/${place._id}`} className="place-card-link">
+         <article className="place-card">
+           <header>
+             <h2>{place.placeName}</h2>
+             <p>
+               {place.user.username} posted on{' '}
+               {new Date(place.createdAt).toLocaleDateString()}
+             </p>
+           </header>
+           <p>{place.description}</p>
+           {place.image && (
+             <div className="upload-image" style={{ backgroundImage: `url(${place.image})` }}> </div>
+           )}
+         </article>
+       </Link>
+     ))}
+   </main>
+ )
 }
+
 
 export default PlaceList
