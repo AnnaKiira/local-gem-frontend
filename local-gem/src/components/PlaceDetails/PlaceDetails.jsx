bd2e8de623c3822ee47d05b2ed1edc3f7654d17f
Ali <belitherali@gmail.com>
2024-08-09 13:59:57 +0100

    styling changes to placeDetails

--- local-gem/src/components/PlaceDetails/PlaceDetails.jsx
+++ local-gem/src/components/PlaceDetails/PlaceDetails.jsx
@@ -1,102 +1,104 @@
-import { useState, useEffect, useContext } from 'react'
-import { Link, useParams } from 'react-router-dom'
-import * as placesService from '../../services/placeService.js'
-import CommentForm from '../CommentForm/CommentForm.jsx'
-import { AuthedUserContext } from '../../App'
-import Container from "react-bootstrap/Container"
-import Row from "react-bootstrap/Row"
-import Col from "react-bootstrap/Col"
-import "./PlaceDetails.scss"
+import { useState, useEffect, useContext } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import * as placesService from '../../services/placeService.js';
+import CommentForm from '../CommentForm/CommentForm.jsx';
+import { AuthedUserContext } from '../../App';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import './PlaceDetails.scss';
 
+const PlaceDetails = ({ handleDeletePlace }) => {
+  const user = useContext(AuthedUserContext);
+  const [place, setPlace] = useState(null);
+  const { placeId } = useParams();
 
-const PlaceDetails = ({ handleDeletePlace}) => {
- 
- const user = useContext(AuthedUserContext)
+  useEffect(() => {
+    const fetchPlace = async () => {
+      const singlePlace = await placesService.show(placeId);
+      setPlace(singlePlace);
+    };
+    fetchPlace();
+  }, [placeId]);
 
+  const handleAddComment = async (formData) => {
+    const newComment = await placesService.createComment(placeId, formData);
 
+    setPlace({
+      ...place,
+      comments: [...place.comments, newComment],
+    });
+  };
 
- const [place, setPlace] = useState(null)
+  if (!place) return <main>Loading...</main>;
 
+  return (
+    <main className="place-details-container">
+      <Container>
+        <Row>
+          <Col xs="12" className="text-center mb-4">
+            <h1 className="place-title">{place.placeName}</h1>
+            <p className="post-details">
+              {place.user.username} posted on{' '}
+              {new Date(place.createdAt).toLocaleDateString()}
+            </p>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs="12" className="text-center">
+            {place.image && (
+              <div
+                className="upload-image"
+                style={{ backgroundImage: `url(${place.image})` }}
+              ></div>
+            )}
+            <p className="place-description">{place.description}</p>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs="12" className="text-center">
+            {place.user._id === user._id && (
+              <div className="action-buttons">
+                <button
+                  className="del-button btn btn-danger"
+                  onClick={() => handleDeletePlace(placeId)}
+                >
+                  Delete Place
+                </button>
+                <Link
+                  className="update-button btn btn-primary"
+                  to={`/places/${placeId}/edit`}
+                >
+                  Update Place
+                </Link>
+              </div>
+            )}
+          </Col>
+        </Row>
+        <Row>
+          <Col xs="12">
+            <h2 className="comments-title">Comments</h2>
+            {!place.comments.length && <p>There are no comments.</p>}
+            {place.comments.map((comment) => (
+              <div key={comment._id} className="comment-card card mb-3">
+                <div className="card-body">
+                  <h6 className="card-title">
+                    {comment.user.username} posted on{' '}
+                    {new Date(comment.createdAt).toLocaleDateString()}
+                  </h6>
+                  <p className="card-text">{comment.text}</p>
+                </div>
+              </div>
+            ))}
+            <div className="add-comment-section">
+              <label htmlFor="comment">Your comment:</label>
+              <CommentForm handleAddComment={handleAddComment} />
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </main>
+  );
+};
 
- 
- const { placeId } = useParams()
-
-
- useEffect(() => {
-   const fetchPlace = async () => {
-     const singlePlace = await placesService.show(placeId)
-     setPlace(singlePlace)
-   }
-   fetchPlace()
- }, [placeId])
-
-
- const handleAddComment = async (formData) => {
-   const newComment = await placesService.createComment(placeId, formData)
-
-
-   setPlace({
-     ...place,
-     comments: [...place.comments, newComment]
-   })
- }
-
- if (!place) return <main>Loading...</main>
-
- return (
-   <main className="placedetails">
-    <Container className="containerplace">
-      <Row>
-      <Col xs="12" className="showtitle">
-       <h1>{place.placeName}</h1>
-       </Col>
-       </Row>
-       <Row>
-       <Col xs="12">
-       <p>
-        posted by {place.user.username}
-       </p>
-
-
-     {place.image && (
-      <div className="upload-image" style={{backgroundImage: `url(${place.image})`}}> </div>)}
-     <p className= "description-card">{place.description}</p>
-</Col>
-</Row>
-<Row className="commentsrow">
-<Col className= "commentsall">
-     <CommentForm handleAddComment={handleAddComment} />
-       {!place.comments.length && <p>There are no comments.</p>}
-
- 
-       {place.comments.map((comment) => (
-                 <article key={comment._id}>
-             <div className="card">  
-             <h6 className="card-title">
-               {comment.user.username} said: 
-             </h6>
-           <p className="card-text">{comment.text}</p>
-           </div>
-           </article>
-
-
-       ))}
-
-     </Col>
-</Row>
-<Row>
-       { place.user._id === user._id &&
-       <section>
-         <button className="del-button" onClick={() => handleDeletePlace(placeId)}>Delete Place</button>
-         <Link className="update-button" to={`/places/${placeId}/edit`}>Update Place</Link>
-       </section>
-     }
-</Row>
-</Container>
-</main>
- 
- )
-}
-
-export default PlaceDetails
-
+export default PlaceDetails;
