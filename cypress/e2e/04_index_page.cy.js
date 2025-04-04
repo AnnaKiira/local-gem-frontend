12c90d4e152a6f7ccf43e2fabf17652e93eb2dbe
User Name <annakirath@gmail.com>
2025-04-04 21:50:50 +0100

    testing the index page with Cypress to assert visibility on uploaded posts

--- cypress/e2e/04_index_page.cy.js
+++ cypress/e2e/04_index_page.cy.js
@@ -12,10 +12,17 @@ describe('The index page should load and display all posts', () => {
         
     })
 
-    
+    it('asserts the cards and their content is visible', () => {
 
-    it('asserts visibility', () => {
+        cy.get('.place-card').should('be.visible') //asserting each place card is visible
 
+        cy.get('.place-card').find('h2').should('be.visible') //asserting the h2's are visible on the cards
+
+        cy.get('header').find('p').should('be.visible') //asserting the p tag in the header section is visible
+
+        cy.get('article').find('p').should('be.visible') //not fully done with this one
+
+        cy.get('.place-card').find('.upload-image').should('be.visible') //asserting the image is visible
     })
 
 })
